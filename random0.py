a720b76483042b589974235cb4d89503ae30c8c6
Denis McLaughlin <mclaughlin.denis@gmail.com>
2016-08-01 21:26:44 -0600

    Changes to balance teams to get 50% euchres
     - previously, the first player to be offered an order would always order,
       which skewed the euchre %
     - now, each team will order the hole card 50% of the time
     - for the non-dealing team, either player will order 25% of the time
     - for the dealing team, the non-dealing player will never order, and the
       dealer will pick up 50% of the time
     - when ordered, the dealer will always drop the ordered card: without this,
       the dealing team has an advantage which skews the euchre % away from 50%
     - if no player orders, then players will randomly call a suit to be trump,
       with 25% probability, except for the dealer who will always call a suit
     - called suit is random amongst the 3 non-hole-card-suit suits
     - there is now a decideCallPass() method to handle calling suits
     - decideDrop() is now passed the hole card, since the decideDrop() method
       can choose to drop the ordered card
     - some output tweaks, to tidy up the logs
     - various comment tweaks to document the above changes

---
+++ random0.py
@@ -0,0 +1,160 @@
+# This class shows a simple implementation of a player class for peuchre:
+#  - it inherits from EuchrePlayer
+#  - it has a class called Player
+#  - it implements the core Player methods:
+#     - decideOrderPass()
+#     - decideCallPass()
+#     - decideDrop()
+#     - decideDefend()
+#     - decidePlayLead()
+#     - decidePlayFollow()
+#
+# This is the basic random player.
+#  - has a 25% of ordering the hole card
+#  - has a 25% of calling a random suit
+#  - if it's the dealer will always call a random suit
+#  - never goes alone
+#  - drops the ordered card
+#  - never defends alone
+#  - leads randomly
+#  - follows randomly
+
+
+import random
+
+from logging import warning as warn, log, debug, info, error, critical
+from card import Card
+from euchreplayer import EuchrePlayer
+
+class Player(EuchrePlayer):
+
+    ###########################################################################
+    #
+    def __init__(self, **kwargs):
+        EuchrePlayer.__init__(self,**kwargs)
+
+
+    ###########################################################################
+    # this routine returns either ORDER, ORDERALONE, or ORDERPASS, depending
+    # on what the client wishes to do with the hole card; this routine returns
+    # ORDER with a probability of 1/4, PASS otherwise
+    #
+    def decideOrderPass(self):
+        # randomly choose to order with 25% probability
+        op = random.choice(["ORDER","ORDERPASS","ORDERPASS","ORDERPASS"])
+
+        # if aloneonorder is true, and we're the dealer's partner, we never
+        # order, since it will force a go alone
+        if self.state['aloneonorder'] == 1:
+            if self.team == self.state[self.state['dealer']]['team']:
+                op = "ORDERPASS"
+            # but if I'm the dealer, we order with 50% probability, to
+            # make up for my partner never making it
+            if self.playerhandle == self.state['dealer']:
+                op = random.choice(["ORDER","ORDERPASS"])
+        
+        info("")
+        info(self.id+"cards: " + self.printHand(self.hand))
+        if op == "ORDER":
+            info(self.id+"I will order " + self.state['hole']
+                + " to " + self.state[ self.state['dealer'] ]['name'] )
+        else:
+            info(self.id+"I will pass on ordering " + self.state['hole'])
+
+        return self.messageId[op]
+
+
+    ###########################################################################
+    # This routine returns either CALL, CALLALONE, or CALLPASS, and a suit,
+    # depending on whether the player whichs to call or call alone a suit,
+    # or pass.  This implementation follows this logic:
+    #  - if we're not the dealer, randomly choose to call (25%) or pass
+    #    (75%)
+    #  - if we choose to call, we randomly choose a suit that isn't
+    #    the hole suit
+    #
+    def decideCallPass(self):
+        # if we're the dealer, we have to call
+        op = "CALL"
+        suit = None
+
+        # if we're not the dealer, call with 25% chance
+        if self.state['dealer'] != self.playerhandle:
+            op = random.choice(["CALL","CALLPASS","CALLPASS","CALLPASS"])
+        
+        info("")
+        info(self.id+"cards: " + self.printHand(self.hand))
+
+        # if we're going to call, get a list of all the suits, remove the
+        # suit of the hole card (since it's been declined) and randomly
+        # choose amongst the remainder
+        if op == "CALL":
+            suits = Card.suits()
+            suits.remove(self.state['hole'].suit)
+            suit = random.choice(suits)
+            info(self.id+"I will call " + Card.suitName(suit))
+        else:
+            info(self.id+"I will pass on calling")
+
+        # returning this dict seems to be the easiest way to return multiple
+        # values out a function in python
+        return {'op':self.messageId[op], 'suit':suit}
+
+
+    ###########################################################################
+    # this routine is called when the client is the dealer and has been
+    # ordered up, and hence must choose a card to drop; hole is the card
+    # that was ordered up
+    #
+    def decideDrop(self, hole):
+        # log our intent
+        info("")
+        info(self.id+"cards: " + self.printHand(self.hand))
+        info(self.id+"dropping ordered card: " + hole)
+        info("")
+
+        return hole
+
+
+    ###########################################################################
+    # this routine is called when the client has been offered the chance to
+    # defend alone; it should return one of DEFEND or DEFENDPASS
+    #
+    def decideDefend(self):
+        return self.messageId['DEFENDPASS']
+
+
+    ###########################################################################
+    # This routine is called when the client is offered the option to lead
+    # a trick; it just returns a random card from the deck
+    #
+    def decidePlayLead(self):
+        # choose a random card from our hand to lead
+        (card,) = random.sample(self.hand,1)
+
+        # log our intent
+        info("")
+        info(self.id+"cards: " + self.printHand(self.hand))
+        info(self.id+"leading with " + card
+            + " (" + self.printHand(self.hand) + ")")
+
+        return card
+
+
+    ###########################################################################
+    # This routine is called when the client has to choose a card to follow
+    # in a trick; it generates a list of valid cards and then returns a
+    # random one of them
+    #
+    def decidePlayFollow(self):
+        # choose a random card from the set of cards that we can follow with
+        info("")
+        info(self.id+"cards: " + self.printHand(self.hand))
+        cards = self.followCards()
+        (card,) = random.sample(cards,1)
+
+        # log our intent
+        info(self.id+"following with " + card
+            + " (" + self.printHand(self.hand) + ")")
+
+        return card
